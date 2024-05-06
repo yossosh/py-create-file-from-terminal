08a032afd9b769a7d30ae22808e6506f5b61604a
Sofiia <sonagm9@gmail.com>
2024-05-06 15:25:06 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,46 +3,36 @@ import sys
 from datetime import datetime
 
 
-def create_folder(args: list) -> str:
-    if "-d" not in args:
-        return ""
-
-    start_index = args.index("-d") + 1
-    end_index = args.index("-f") if "-f" in args[start_index:] else len(args)
-    paths = args[start_index:end_index]
-    path = os.path.join(*paths)
-
-    os.makedirs(path, exist_ok=True)
-    return path
-
-
-def create_file(args: str) -> None:
-    if "-f" not in args:
-        return
-
-    path = create_folder(args)
-    file_name = os.path.join(path, args[args.index("-f") + 1])
-    open_mode = "a" if os.path.exists(file_name) else "w"
-
-    with open(file_name, open_mode) as file:
-        if open_mode == "a":
-            file.write("\n")
-
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        file.write(f"{current_time}\n")
-
-        line_count = 0
+def file_content_printer(current_file: str) -> None:
+    with open(current_file, "a") as new_file:
+        new_file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+        counter = 1
         while True:
-            line_count += 1
-            line_input = input("Enter content line: ")
-            if line_input == "stop":
+            line = input("Enter content line: ")
+            if "stop" in line:
                 break
-            file.write(f"{line_count} {line_input}\n")
+            new_file.write(f"{counter} {line}\n")
+            counter += 1
+        new_file.write("\n")
+
+
+def create_path(filename: str = None) -> str:
+    dirs = filter(lambda x: "." not in x and x not in ["-f", "-d"], sys.argv)
+    if not dirs:
+        return
+    return os.path.join(*dirs, filename or "")
 
 
 def main() -> None:
-    arguments = sys.argv
-    create_file(arguments)
+    if "-f" in sys.argv and "-d" in sys.argv:
+        file_name = sys.argv[sys.argv.index("-f") + 1]
+        os.makedirs(create_path(), exist_ok=True)
+        file_content_printer(create_path(file_name))
+    elif sys.argv[1] == "-f":
+        file_name = sys.argv[sys.argv.index("-f") + 1]
+        file_content_printer(file_name)
+    elif sys.argv[1] == "-d":
+        os.makedirs(create_path(), exist_ok=True)
 
 
 if __name__ == "__main__":
